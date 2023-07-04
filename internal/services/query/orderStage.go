d695798b00dd6cd0fb03d61f04e5030fb105fe12
Pol Bachelin <pol.bachelin@epitech.eu>
2023-07-04 15:09:25 +0200

    added order stage

---
+++ internal/services/query/orderStage.go
@@ -0,0 +1,25 @@
+package query
+
+import "go.mongodb.org/mongo-driver/bson"
+
+func generateOrderStage(order Order) bson.M {
+	sort := bson.M{}
+	for i, dimension := range order.DimensionName {
+		sort[dimension] = getOrderType(order.DimensionOrder[i])
+	}
+	for i, measure := range order.MeasureName {
+		sort[measure] = getOrderType(order.MeasureOrder[i])
+	}
+	return bson.M{"$sort": sort}
+}
+
+func getOrderType(orderType string) int {
+	switch orderType {
+	case "asc":
+		return 1
+	case "desc":
+		return -1
+	default:
+		return 1
+	}
+}
